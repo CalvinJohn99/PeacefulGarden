0a648429ba5fdc04ba4411e7d07014da0cf70539
Calvin John <calvindegr8@gmail.com>
2021-09-17 08:46:19 +0400

    ELIFECYCLE fix - Node_Modules setup and update to firebase

--- App.js
+++ App.js
@@ -6,28 +6,26 @@ import 'firebase/database';
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMUuAhdumTGYMSVuWjRQmKSRKJhONusAg",
-  authDomain: "peacefulgarden-a4b5c.firebaseapp.com",
-  databaseURL: "https://peacefulgarden-a4b5c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "peacefulgarden-a4b5c",
-  storageBucket: "peacefulgarden-a4b5c.appspot.com",
-  messagingSenderId: "595789693308",
-  appId: "1:595789693308:web:6c2e1faf2651b5c8e924f9",
-  measurementId: "G-45KC4GX795"
+  apiKey: "AIzaSyBvxZ3XGNOmsp1vXdM4Lh-Wcai2fe8ECNQ",
+  authDomain: "gratefulness-posts.firebaseapp.com",
+  projectId: "gratefulness-posts",
+  storageBucket: "gratefulness-posts.appspot.com",
+  messagingSenderId: "204123862413",
+  appId: "1:204123862413:web:52aa285ef04de2ccb639c4"
 };
 
 // initialize firebase
 // expo will create the database whenever we save the file. In order to avoid the duplicates of database creation and errors, we use an if condition to control that.
-if(firebase.apps.length === 0) {
-firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
 // Get a database reference
 const fbdata = firebase.database();
 
 function App() {
-  return(
-    <MainContainer/>
+  return (
+    <MainContainer />
   );
 }
 
