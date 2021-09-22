62a9b19d8771780787b5f81ab98a48207e259afc
VivianApple <90442095+VivianApple@users.noreply.github.com>
2021-09-22 20:12:21 +1000

    self-awareness question - 1

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,75 +1,122 @@
-import * as React from 'react';
+import * as React from "react";
 
 // import navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 // import screens
-import HomeScreen from './screens/HomeScreen';
-import GPostScreen from './screens/GPostScreen';
-import QuestionScreen from './screens/QuestionScreen';
-import JournalMoodScreen from './screens/JournalMoodScreen';
-import MusicScreen from './screens/MusicScreen';
-import AccountScreen from './screens/AccountScreen';
+import HomeScreen from "./screens/HomeScreen";
+import GPostScreen from "./screens/GPostScreen";
+import QuestionScreen from "./screens/QuestionScreen";
+import QuestionViewAnswer from "./screens/QuestionViewAnswer";
+import AnswerQuestion from "./screens/AnswerQuestion";
+import JournalMoodScreen from "./screens/JournalMoodScreen";
+import MusicScreen from "./screens/MusicScreen";
+import AccountScreen from "./screens/AccountScreen";
 
-// Create screen names
-//const homeName = 'Home';
-//const DetailsName = 'Details';
-//const SettingsName = 'Settings';
+// Create Self-awareness Question stack
+const QuesStack = createStackNavigator();
+const QuesStackScreen = () => {
+  return (
+    <QuesStack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#1067CC" },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <QuesStack.Screen
+        name="QList"
+        component={QuestionScreen}
+        options={{ headerTitle: "Question List" }}
+      />
+      <QuesStack.Screen
+        name="QViewAnswer"
+        component={QuestionViewAnswer}
+        options={{ headerTitle: "View Answer" }}
+      />
+      <QuesStack.Screen
+        name="QCreateAnswer"
+        component={AnswerQuestion}
+        options={{ headerTitle: "Post Answer" }}
+      />
+    </QuesStack.Navigator>
+  );
+};
 
 // Create tab
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    return (
-        <Tab.Navigator screenOptions={ ({route}) => ({
-            headerStyle: {
-                backgroundColor: "#1067CC",
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-                fontWeight: 'bold'
-            }, 
-            tabBarIcon: ({focused, color, size}) => {
-                let iconName;
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerStyle: {
+          backgroundColor: "#1067CC",
+        },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-                if (route.name === "Home") {
-                    iconName = focused? 'home' : 'home-outline';
-                } else if (route.name === "Post") {
-                    iconName = focused? 'earth' : 'earth-outline';
-                } else if (route.name === "Question") {
-                    iconName = focused? 'bulb' : 'bulb-outline';
-                } else if (route.name === "Journal") {
-                    iconName = focused? 'book' : 'book-outline';
-                } else if (route.name === "Music") {
-                    iconName = focused? 'musical-notes' : 'musical-notes-outline';
-                } else if (route.name === "Account") {
-                    iconName = focused? 'person' : 'person-outline';
-                }
-                return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'white',
-            tabBarStyle: {
-                backgroundColor: "#1067CC",
-            },
-        })}>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Post" component={GPostScreen} />
-            <Tab.Screen name="Question" component={QuestionScreen} />
-            <Tab.Screen name="Journal" component={JournalMoodScreen} />
-            <Tab.Screen name="Music" component={MusicScreen} />
-            <Tab.Screen name="Account" component={AccountScreen} />
-
-        </Tab.Navigator>
-    );
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Post") {
+            iconName = focused ? "earth" : "earth-outline";
+          } else if (route.name === "Question") {
+            iconName = focused ? "bulb" : "bulb-outline";
+          } else if (route.name === "Journal") {
+            iconName = focused ? "book" : "book-outline";
+          } else if (route.name === "Music") {
+            iconName = focused ? "musical-notes" : "musical-notes-outline";
+          } else if (route.name === "Account") {
+            iconName = focused ? "person" : "person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: "#1067CC",
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Post"
+        component={GPostScreen}
+        options={{ headerTitle: "Gratefulness Post" }}
+      />
+      <Tab.Screen
+        name="Question"
+        component={QuesStackScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Journal"
+        component={JournalMoodScreen}
+        options={{ headerTitle: "Journal and Mood" }}
+      />
+      <Tab.Screen
+        name="Music"
+        component={MusicScreen}
+        options={{ headerTitle: "Relaxing Music" }}
+      />
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Tab.Navigator>
+  );
 }
 
 export default function MainContainer() {
-    return(
-        <NavigationContainer>
-            <MyTabs />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
--- navigation/screens/QuestionScreen.js
+++ navigation/screens/QuestionScreen.js
@@ -1,98 +1,99 @@
-import * as React from 'react';
-import { useState, useEffect} from "react";
-import {SafeAreaView, View, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
-import fbdata from '../../firebase.js';
+import fbdata from "../../firebase.js";
 
-import useCurrentDate from '../components/CommonFunctions.js';
+import useCurrentDate from "../components/CommonFunctions.js";
 
+const getBackgroundColor = (id) => {
+  if (id % 3 === 1) {
+    return "#B6E4CB";
+  } else if (id % 3 === 2) {
+    return "#B5CBDF";
+  } else if (id % 3 === 0) {
+    return "#E8D8D8";
+  }
+};
 
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-    <TouchableOpacity
-    style={[styles.item, backgroundColor]}
-    onPress={onPress}
-    >
-        <Text style={[styles.question, textColor]}> {item.id}. {item.question} </Text>
-    </TouchableOpacity>
-  );
-
-
-export default function QuestionScreen({navigation}) {
-
-    const [QList, setQList] = useState([]);
-    React.useEffect(() => {
-      const questionRef = fbdata.ref('/sa-question').orderByChild('id');
-      const OnLoadingListener = questionRef.once('value', snapshot => {
-        setQList([]);
-        snapshot.forEach((childSnapshot) => {
-          setQList(QList=>[...QList,childSnapshot.val()]);
-          console.log(childSnapshot.val());
-        });
+export default function QuestionScreen({ navigation }) {
+  const [QList, setQList] = useState([]);
+  React.useEffect(() => {
+    const questionRef = fbdata.ref("/sa-question").orderByChild("id");
+    const OnLoadingListener = questionRef.once("value", (snapshot) => {
+      setQList([]);
+      snapshot.forEach((childSnapshot) => {
+        setQList((QList) => [...QList, childSnapshot.val()]);
+        console.log(childSnapshot.val());
       });
-      return () => {
-        questionRef.off();
-      }
-    },[]);
-
-    const [selectedId, setSelectedId] = useState(null);
-
-    const renderItem = ({ item }) => {
-
-      const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#B6E4CB";
-      const color = item.id === selectedId ? 'white' : 'black';
-      return (
-          <Item 
-              item={item}
-              onPress={() => setSelectedId(item.id)}
-              backgroundColor={{backgroundColor}}
-              textColor={{color}}
-          />
-      );
+    });
+    return () => {
+      questionRef.off();
     };
+  }, []);
 
-    const currentDate = useCurrentDate();
+  const currentDate = useCurrentDate();
 
-    return(
-        <SafeAreaView style={styles.container}>
-            <View>
-                <Text style={{top: 10, fontWeight: "bold", fontSize: 26}}> {currentDate} </Text>
-            </View>
-          
-            <FlatList
-                style={{top: 20}}
-                data={QList}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-                extraData={selectedId}
-            />
-            {/*
-            <TouchableOpacity
-                style={{backgroundColor: "#1067CC", padding: 20}}
-                //onPress={() => navigation.navigate('Post')}
-                >
-                    <Text style={{textAlign: 'center', fontWeight: 'bold', color: 'white'}}>More Questions?</Text>
-            </TouchableOpacity> */}
-            
-        </SafeAreaView>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={{ top: 10, fontWeight: "bold", fontSize: 26 }}>
+          {" "}
+          {currentDate}{" "}
+        </Text>
+      </View>
 
-    );
+      <FlatList
+        style={{ top: 20 }}
+        data={QList}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[
+              styles.item,
+              { backgroundColor: getBackgroundColor(item.id) },
+            ]}
+            onPress={() => {
+              navigation.navigate("Question", {
+                screen: "QViewAnswer",
+                params: { item },
+              });
+              console.log({ item });
+            }}
+          >
+            <Text style={styles.question}>
+              {" "}
+              {item.id}. {item.question}{" "}
+            </Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      ></FlatList>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 20, 
-        alignItems: 'center',
-    },
-    item: {
-        margin: 20,
-        padding: 20,
-        backgroundColor: "#B6E4CB",
-        borderRadius: 20,
-    },
-    question: {
-        fontSize: 26,
-        textAlign: 'center', 
-        fontWeight: 'bold'
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 20,
+    alignItems: "center",
+  },
+  item: {
+    margin: 20,
+    padding: 20,
+    borderRadius: 20,
+  },
+  question: {
+    fontSize: 26,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
 });
