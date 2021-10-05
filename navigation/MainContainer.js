3ce69a55177aa243271c6f0671d56cfc1d427755
nghiatieu300120 <55471979+EvansH1997@users.noreply.github.com>
2021-10-05 22:23:52 +0700

    New

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -81,107 +81,108 @@ const JMStackScreen = () => {
 
 // Create tab
 const Tab = createBottomTabNavigator();
+
 function MyTabs() {
-    return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerStyle: {
-            backgroundColor: "#1067CC",
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-  
-            if (route.name === "Home") {
-              iconName = focused ? "home" : "home-outline";
-            } else if (route.name === "Post") {
-              iconName = focused ? "earth" : "earth-outline";
-            } else if (route.name === "Question") {
-              iconName = focused ? "bulb" : "bulb-outline";
-            } else if (route.name === "Journal") {
-              iconName = focused ? "book" : "book-outline";
-            } else if (route.name === "Music") {
-              iconName = focused ? "musical-notes" : "musical-notes-outline";
-            } else if (route.name === "Account") {
-              iconName = focused ? "person" : "person-outline";
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerStyle: {
+          backgroundColor: "#1067CC",
+        },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Post") {
+            iconName = focused ? "earth" : "earth-outline";
+          } else if (route.name === "Question") {
+            iconName = focused ? "bulb" : "bulb-outline";
+          } else if (route.name === "Journal") {
+            iconName = focused ? "book" : "book-outline";
+          } else if (route.name === "Music") {
+            iconName = focused ? "musical-notes" : "musical-notes-outline";
+          } else if (route.name === "Account") {
+            iconName = focused ? "person" : "person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: "#1067CC",
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Post"
+        component={GPostScreen}
+        options={{ headerTitle: "Gratefulness Post" }}
+      />
+      <Tab.Screen
+        name="Question"
+        component={QuesStackScreen}
+        options={{ headerShown: false }}
+        listeners={({ navigation, route }) => ({
+          tabPress: (e) => {
+            // work with transition delay
+            // navigation.dispatch(
+            //   CommonActions.reset({
+            //     index: 0,
+            //     routes: [{ name: "QList" }],
+            //   })
+            // );
+
+            // work with transition delay
+            // navigation.reset({
+            //   index: 0,
+            //   routes: [{ name: "QList" }],
+            // });
+
+            // work with development-only error -- action pop_to_top was not handled by any navigator
+            // navigation.dispatch(StackActions.popToTop());
+
+            const routeName = getFocusedRouteNameFromRoute(route) ?? "QList";
+            if (routeName !== "QList") {
+              navigation.dispatch(StackActions.popToTop());
             }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "white",
-          tabBarInactiveTintColor: "white",
-          tabBarStyle: {
-            backgroundColor: "#1067CC",
           },
         })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen
-          name="Post"
-          component={GPostScreen}
-          options={{ headerTitle: "Gratefulness Post" }}
-        />
-        <Tab.Screen
-          name="Question"
-          component={QuesStackScreen}
-          options={{ headerShown: false }}
-          listeners={({ navigation, route }) => ({
-            tabPress: (e) => {
-              // work with transition delay
-              // navigation.dispatch(
-              //   CommonActions.reset({
-              //     index: 0,
-              //     routes: [{ name: "QList" }],
-              //   })
-              // );
-  
-              // work with transition delay
-              // navigation.reset({
-              //   index: 0,
-              //   routes: [{ name: "QList" }],
-              // });
-  
-              // work with development-only error -- action pop_to_top was not handled by any navigator
-              // navigation.dispatch(StackActions.popToTop());
-  
-              const routeName = getFocusedRouteNameFromRoute(route) ?? "QList";
-              if (routeName !== "QList") {
-                navigation.dispatch(StackActions.popToTop());
-              }
-            },
-          })}
-        />
-        <Tab.Screen
-          name="Journal"
-          component={JMStackScreen}
-          options={{ headerShown: false }}
-          listeners={({ navigation, route }) => ({
-            tabPress: (e) => {
-              // navigation.dispatch(StackActions.popToTop());
-  
-              const routeName = getFocusedRouteNameFromRoute(route) ?? "JMNav";
-              if (routeName !== "JMNav") {
-                navigation.dispatch(StackActions.popToTop());
-              }
-            },
-          })}
-        />
-        <Tab.Screen
-          name="Music"
-          component={MusicScreen}
-          options={{ headerTitle: "Relaxing Music" }}
-        />
-        <Tab.Screen name="Account" component={AccountScreen} />
-      </Tab.Navigator>
-    );
-  }
+      />
+      <Tab.Screen
+        name="Journal"
+        component={JMStackScreen}
+        options={{ headerShown: false }}
+        listeners={({ navigation, route }) => ({
+          tabPress: (e) => {
+            // navigation.dispatch(StackActions.popToTop());
+
+            const routeName = getFocusedRouteNameFromRoute(route) ?? "JMNav";
+            if (routeName !== "JMNav") {
+              navigation.dispatch(StackActions.popToTop());
+            }
+          },
+        })}
+      />
+      <Tab.Screen
+        name="Music"
+        component={MusicScreen}
+        options={{ headerTitle: "Relaxing Music" }}
+      />
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function MainContainer() {
-    return(
-        <NavigationContainer>
-            <MyTabs />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
