afb0c244fd7b80cd1d3bc33e538a42666b1ba5a8
VivianApple <90442095+VivianApple@users.noreply.github.com>
2022-07-03 15:24:12 +0800

    Update AccountScreen.js

--- navigation/screens/AccountScreen.js
+++ navigation/screens/AccountScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-
 import AccountInfo from "./Account/AccountInfo";
 
+// Account screen
+// call componenet <AccountInfo>
+// pass navigation
 function AccountScreen({ navigation }) {
   return <AccountInfo navigation={navigation}></AccountInfo>;
 }
